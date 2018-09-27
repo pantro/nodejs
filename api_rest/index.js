5e28aebd79072809bf04c0b2691030aefc903250
Luna <francoguns16@gmail.com>
2018-09-27 09:02:32 -0500

    GET para uno y vara todos los productos

--- api_rest/index.js
+++ api_rest/index.js
@@ -23,11 +23,23 @@ app.get('/hola/:name', (req, res) => {
 //--- fin ejemplo
 
 app.get('/api/product', (req, res) =>{
-	res.send(200, {products: []})
+	Product.find({}, (err, products) => {
+		if (err) return res.status(500).send({ message: `Error al realizar la petición: ${err}`})
+		if (!products) return res.status(404).send({ message: `No existen productos`})
+
+		res.status(200).send({ products })//cuando la variable y el nombre se llaman iguales puede ir solo uno ({product})
+	})
 })
 //Para acceder a un solo producto
 app.get('/api/product/:productId', (req, res) =>{
-	
+	let productId = req.params.productId
+
+	Product.findById( productId, (err, product) => {
+		if (err) return res.status(500).send({ message: `Error al realizar la petición: ${err}`})
+		if (!product) return res.status(404).send({ message: `El producto no existe`})
+
+		res.status(200).send({ product: product })//cuando la variable y el nombre se llaman iguales puede ir solo uno ({product})
+	})
 })
 //Para subir nuevos productos
 app.post('/api/product', (req, res) =>{
