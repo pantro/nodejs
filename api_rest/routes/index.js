c3820eae457de18a645f99d6d95fdff7c89a6ee5
Luna <francoguns16@gmail.com>
2018-09-28 11:50:50 -0500

    creando middlewares

--- api_rest/routes/index.js
+++ api_rest/routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const ProductCtrl = require('../controllers/product')
+const auth = require('../middlewares/auth')
 const api = express.Router()
 
 //PETICIONES
@@ -14,5 +15,9 @@ api.post('/product', ProductCtrl.saveProduct)
 api.put('/product/:productId', ProductCtrl.updateProduct)
 //Para eliminar
 api.delete('/product/:productId', ProductCtrl.deleteProduct)
+
+api.get('/private', auth.isAuth, function (req, res) {
+	res.status(200).send({ message: 'Tienes acceso' })
+})
 
 module.exports = api
